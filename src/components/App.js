cc1e84f23cc9fe1861cf0097af9708299c023b66
Vedprakash Kumar <131158289+Vedprakash321@users.noreply.github.com>
2024-04-25 10:07:39 +0000

    new solution

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,40 @@ import UseMemo from './ReactMemo';
 import '../styles/App.css';
 
 const App = () => {
+  
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [customTask, setCustomTask] = useState('');
+
+  useEffect(() => {
+    // Any side effects can be placed here
+  }, []);
+
+  const handleAddTodo = () => {
+    setTasks([...tasks, 'New todo']);
+  };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleCustomTaskChange = (e) => {
+    setCustomTask(e.target.value);
+  };
+
+  const handleCustomTaskSubmit = () => {
+    if (customTask.length > 5) {
+      setTasks([...tasks, customTask]);
+      setCustomTask('');
+    } else {
+      alert('Task must be more than 5 characters.');
+    }
+  };
+
+
+
+
   return (
     <div id="main">
       <button onClick={handleAddTodo}>My todo</button>
