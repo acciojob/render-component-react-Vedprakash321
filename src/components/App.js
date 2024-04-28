8bbb0f269b7859c6ae6ad6d06d20eeb25432c755
Vedprakash Kumar <131158289+Vedprakash321@users.noreply.github.com>
2024-04-28 09:08:10 +0000

    solution added

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,14 @@
-import React, {Component, useEffect, useState} from "react";
-import UseMemo from './useMemo';
-import UseMemo from './ReactMemo';
-import '../styles/App.css';
+import React from 'react';
+
 
 const App = () => {
-  
-function App() {
-  const [tasks, setTasks] = useState([]);
-  const [counter, setCounter] = useState(0);
-  const [customTask, setCustomTask] = useState('');
-
-  useEffect(() => {
-    // Any side effects can be placed here
-  }, []);
-
-  const handleAddTodo = () => {
-    setTasks([...tasks, 'New todo']);
-  };
-
-  const handleIncrement = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleCustomTaskChange = (e) => {
-    setCustomTask(e.target.value);
-  };
-
-  const handleCustomTaskSubmit = () => {
-    if (customTask.length > 5) {
-      setTasks([...tasks, customTask]);
-      setCustomTask('');
-    } else {
-      alert('Task must be more than 5 characters.');
-    }
-  };
-
-
-
 
   return (
-    <div id="main">
-      <button onClick={handleAddTodo}>My todo</button>
-      <button onClick={handleIncrement}>Increment</button>
-      <div>
-        <input
-          type="text"
-          value={customTask}
-          onChange={handleCustomTaskChange}
-          placeholder="Enter custom task"
-        />
-        <button onClick={handleCustomTaskSubmit}>Submit</button>
-      </div>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
-      <p>Counter: {counter}</p>
-      <UseMemo />
-      <ReactMemo />
-    </div>
+    <h1>
+     
+      <p>I am learning React. My life is getting better.</p>
+      
+    </h1>
   );
-  )
-}
-
-
+};
 export default App;
